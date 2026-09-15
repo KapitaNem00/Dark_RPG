147dbfb97ed08b0129d9672a52e786ccd06b4d96
Матвеев Артемий <matveev_artemij@list.ru>
2026-09-15 18:24:05 +0300

    Led 6

--- pwm-led.py
+++ pwm-led.py
@@ -6,13 +6,21 @@ GPIO.setmode(GPIO.BCM)
 led = 26
 dat = 6
 
+
 GPIO.setup(led, GPIO.OUT)
 GPIO.setup(dat, GPIO.IN)
+
+pwm = GPIO.PWM(led, 200)
+duty = 0.0
+pwm.start(duty)
 
 state = 0
 period = 1.0
 
 while True:
-    state = not GPIO.input(dat)
-    GPIO.output(led, state)
-    time.sleep(0.2)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(0.05)
+    
+    duty += 1.0
+    if duty > 100.0:
+        duty = 0.0
