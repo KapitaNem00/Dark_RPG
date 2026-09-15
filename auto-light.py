6f8c1d7dbc4b19520095e653e8fbe07cb0f4d869
Матвеев Артемий <matveev_artemij@list.ru>
2026-09-15 18:16:19 +0300

    Led 3

---
+++ auto-light.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+led = 26
+dat = 6
+
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(dat, GPIO.IN)
+
+state = 0
+period = 1.0
+
+while True:
+    state = GPIO.input(dat)
+    GPIO.output(led, state)
+    time.sleep(0.2)
