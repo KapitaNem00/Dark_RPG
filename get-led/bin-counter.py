b4fcfae6703eb60573c4746c795c23db75cd006d
Матвеев Артемий <matveev_artemij@list.ru>
2026-09-22 17:54:31 +0300

    bin-c

--- get-led/bin-counter.py
+++ get-led/bin-counter.py
@@ -7,8 +7,8 @@ leds = [24, 22, 23, 27, 17, 25, 12, 16]
 leds.reverse()
 num = 0
 sleep_time = 0.2
-up=10
-down=9
+up=9
+down=10
 
 
 GPIO.setup(leds, GPIO.OUT)
@@ -24,8 +24,8 @@ while True:
         num = num + 1
         print(num, dec2bin(num))
         time.sleep(sleep_time)
-    if GPIO.input(up):
-            num = num + 1
+    if GPIO.input(down):
+            num = num - 1
             print(num, dec2bin(num))
             time.sleep(sleep_time)
     GPIO.output(leds, dec2bin(num))
