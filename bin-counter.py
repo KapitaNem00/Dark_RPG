39537504b8bb52aa69c0a04c026b4fee4fe8883d
Матвеев Артемий <matveev_artemij@list.ru>
2026-09-22 17:45:49 +0300

    bin-c

--- bin-counter.py
+++ bin-counter.py
@@ -6,10 +6,13 @@ GPIO.setmode(GPIO.BCM)
 leds = [24, 22, 23, 27, 17, 25, 12, 16]
 num = 0
 sleep_time = 0.2
+up=10
+down=9
 
 
 GPIO.setup(leds, GPIO.OUT)
-
+GPIO.setup(up, GPIO.IN)
+GPIO.setup(down, GPIO.IN)
 GPIO.output(leds, 0)
 
 def dec2bin(value):
@@ -20,4 +23,8 @@ while True:
         num = num + 1
         print(num, dec2bin(num))
         time.sleep(sleep_time)
+    if GPIO.input(up):
+            num = num + 1
+            print(num, dec2bin(num))
+            time.sleep(sleep_time)
     GPIO.output(leds, dec2bin(num))
